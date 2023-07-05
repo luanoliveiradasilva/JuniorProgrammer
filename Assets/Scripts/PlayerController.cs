34c4d8df5309164a80cdb4fb7e2b2bd388bc812a
Luan Nascimento Oliveira <luanoliveira1107@gmail.com>
2023-07-05 18:13:14 +0100

    Car
    Move the car in directions forward, left and right. Add move speed.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeedVeichle = 0.2f;
+    private float moveSpeedVeichle = 10.0f;
+    private float turnSpeed = 25.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,12 +17,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float veticalInput = Input.GetAxis("Vertical");
+
         // We´ll  move the veichle forward.
         //Vector3.forward Explanation : https://docs.unity3d.com/ScriptReference/Vector3.html
         var movePlayer = Vector3.forward;
+        var moveVector = Vector3.up;
 
         //Time.deltaTime explanation: https://docs.unity3d.com/ScriptReference/Time-deltaTime.html
-        transform.Translate(movePlayer * moveSpeedVeichle * Time.deltaTime);        
+        //Move the car direction forward with input vertical input.
+        transform.Translate(movePlayer * moveSpeedVeichle * Time.deltaTime * veticalInput);
+
+        //Move the car in direction up with rotate left and right.
+        transform.Rotate(moveVector * Time.deltaTime * turnSpeed * horizontalInput);
+        
 
     }
 }

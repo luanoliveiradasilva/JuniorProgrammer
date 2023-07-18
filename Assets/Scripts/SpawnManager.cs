ddfd2bb4085800495db67ee558b8dc3b1f8eb1ab
Luan Nascimento Oliveira <luanoliveira1107@gmail.com>
2023-07-18 17:16:47 +0100

    Lesson 2.3 - Random Animal Stampede
    1.Create a spawn manager
    2.Spawn an animal if S is pressed
    3.Spawn random animals from array
    4.Randomize the spawn location
    5.Change the perspective of the camera

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{ 
+    internal class SpawnManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] animalPrefabs;
+        private float spawnRangeX = 20.0f;
+        private float spawnPositionZ = 20.0f;
+
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                //Lenght animals with Random.
+                int animalIndex = Random.Range(0, animalPrefabs.Length);
+
+                //Position animal with ranfom.
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPositionZ);
+
+                //Instantiate animal with prefabs 
+                Instantiate(animalPrefabs[animalIndex], spawnPosition, animalPrefabs[animalIndex].transform.rotation);
+            }
+            
+        }
+    }
+}

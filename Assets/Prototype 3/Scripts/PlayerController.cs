f158edcd1f481334f94f9813cc6b4e32ae0f16be
Luan Nascimento Oliveira <luanoliveira1107@gmail.com>
2023-07-24 16:45:41 +0100

    Lesson 3.2 - Make the World Whiz By / Lesson 3.3 - Don't Just Stand There
    1.Create a script to repeat background
    2.Reset position of background
    3.Fix background repeat with collider
    4.Add a new game over trigger
    5.Stop MoveLeft on gameOver
    6.Stop obstacle spawning on gameOver
    7.Destroy obstacles that exit bounds
    
    1.Explore the player’s animations
    2.Make the player start off at a run
    3.Set up a jump animation
    4.Adjust the jump animation
    5.Set up a falling animation
    6.Keep player from unconscious jumping

--- Assets/Prototype 3/Scripts/PlayerController.cs
+++ Assets/Prototype 3/Scripts/PlayerController.cs
@@ -6,27 +6,40 @@ namespace Assets.Prototype_3.Scripts
 {
     public class PlayerController : MonoBehaviour
     {
-
+        private Animator playerAnimation;
         private Rigidbody rigidBody;
+        private AudioSource audioSource;
+
+        [SerializeField] private ParticleSystem explosionParticle;
+        [SerializeField] private ParticleSystem dirtParticle;
+        [SerializeField] private AudioClip crashSound;
+        [SerializeField] private AudioClip jumpSound;
         [SerializeField] private float jumpForce;
         [SerializeField] private float gravityModifier;
         [SerializeField] private bool isOnGround = true;
+
         public bool gameOver = false;
 
         // Start is called before the first frame update
         void Start()
         {
             rigidBody = GetComponent<Rigidbody>();
+            playerAnimation = GetComponent<Animator>();
+            audioSource = GetComponent<AudioSource>();
+
             Physics.gravity *= gravityModifier;
         }
 
         // Update is called once per frame
         void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+            if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
             {
                 rigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
                 isOnGround = false;
+                playerAnimation.SetTrigger("Jump_trig");
+                dirtParticle.Stop();
+                audioSource.PlayOneShot(jumpSound, 1.0f);
             }
         }
 
@@ -35,12 +48,18 @@ namespace Assets.Prototype_3.Scripts
             if (collision.gameObject.CompareTag("Ground"))
             {
                 isOnGround = true;
-
+                dirtParticle.Play();
             }
             else if (collision.gameObject.CompareTag("Obstacle"))
             {
                 gameOver = true;
                 Debug.Log("Game Over");
+
+                playerAnimation.SetBool("Death_b", true);
+                playerAnimation.SetInteger("DeathType_int", 1);
+                explosionParticle.Play();
+                dirtParticle.Stop();
+                audioSource.PlayOneShot(crashSound, 1.0f);
             }
         }
     }
